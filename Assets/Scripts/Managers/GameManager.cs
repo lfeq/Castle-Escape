03a2f1ae757454c2b54eef94d3995e175520a35f
lfeq <panchocosa@gmail.com>
2023-06-13 12:46:13 -0600

    Level 1, 2 and 3. Created game manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,86 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+  public static GameManager s_instance;
+  
+  private GameState m_gameState;
+
+    private void Awake() {
+        if(FindObjectOfType<GameManager>() != null && 
+            FindObjectOfType<GameManager>().gameObject != gameObject) {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        s_instance = this;
+        m_gameState = GameState.None;
+    }
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
+
+    public void changeGameSate(GameState t_newState) {
+        if(m_gameState == t_newState) {
+            return;
+        }
+        m_gameState = t_newState;
+        switch (m_gameState) {
+            case GameState.None:
+                break;
+            case GameState.LoadMainMenu:
+                break;
+            case GameState.MainMenu:
+                break;
+            case GameState.LoadLevel:
+                startGame();
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.GameOver:
+                gameOver();
+                break;
+            default:
+                throw new UnityException("Invalid Game State");
+        }
+    }
+
+    public void changeGameStateInEditor(string t_newState) {
+        changeGameSate((GameState)System.Enum.Parse(typeof(GameState), t_newState));
+    }
+    
+    public void startGame() {
+       
+    }
+
+    public GameState getGameState() {
+        return m_gameState;
+    }
+
+    public void restartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver() {
+
+    }
+}
+
+public enum GameState {
+    None,
+    LoadMainMenu,
+    MainMenu,
+    LoadLevel,
+    Playing,
+    GameOver
 }
